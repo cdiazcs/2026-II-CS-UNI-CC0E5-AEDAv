b54034eb47299b5ec6e37a1e1a62f39a55b8ed52
Ernesto Cuadros-Vargas <ecuadros@gmail.com>
2026-09-08 20:50:20 -0500

    Vector 0.3

--- containers/vector.h
+++ containers/vector.h
@@ -1,17 +1,21 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
 #include <iostream>
 #include <stdexcept>
 #include <algorithm> // para std::swap
 
+using T = int;
 class Vector {
 private:
-    int* data,
+    T  * data,
          m_size,      // cantidad actual
          m_capacity;  // capacidad
 
     void reserve(int new_cap) {
         if (new_cap <= m_capacity) return;
-        int* new_data = new int[new_cap];
-        for (int i = 0; i < m_size; ++i) {
+        T* new_data = new T[new_cap];
+        for (size_t i = 0; i < m_size; ++i) {
             new_data[i] = data[i];
         }
         delete[] data;
@@ -28,7 +32,7 @@ public:
 
     Vector(const Vector& other) : data(nullptr), m_size(0), m_capacity(0) {
         reserve(other.m_size);
-        for (int i = 0; i < other.m_size; ++i) {
+        for (size_t i = 0; i < other.m_size; ++i) {
             data[i] = other.data[i];
         }
         m_size = other.m_size;
@@ -54,7 +58,7 @@ public:
         std::swap(m_capacity, other.m_capacity);
     }
 
-    void push_back(int value) {
+    void push_back(T value) {
         if (m_size == m_capacity) {
             int new_cap = (m_capacity == 0) ? 1 : m_capacity * 2;
             reserve(new_cap);
@@ -66,36 +70,38 @@ public:
     void pop_back() {
         if (m_size > 0) {
             --m_size;
-            // data[m_size].~int();
+            // data[m_size].~T();
         }
     }
 
-    int& operator[](int index) {
+    T& operator[](int index) {
         return data[index];
     }
 
-    const int& operator[](int index) const {
+    const T& operator[](int index) const {
         return data[index];
     }
 
-    int& at(int index) {
+    T& at(int index) {
         if (index >= m_size) throw std::out_of_range("Indice fuera de rango");
         return data[index];
     }
 
-    const int& at(int index) const {
+    const T& at(int index) const {
         if (index >= m_size) throw std::out_of_range("Indice fuera de rango");
         return data[index];
     }
 
-    int size() const { return m_size; }
-    int capacity() const { return m_capacity; }
+    T size() const { return m_size; }
+    T capacity() const { return m_capacity; }
     bool empty() const { return m_size == 0; }
 
     void clear() {
-        for (int i = 0; i < m_size; ++i) {
-            // data[i].~int();
+        for (size_t i = 0; i < m_size; ++i) {
+            // data[i].~T();
         }
         m_size = 0;
     }
 };
+
+#endif // __VECTOR_H__
